974eb9be08805ce8738b3c45697f473eacdcc2e0
usuario_local <usuario_local@pto0206>
2020-02-18 14:29:09 +0100

    Primer commit jeje :)

---
+++ ucmh/src/main/java/es/ucm/fdi/iw/UcmhApplication.java
@@ -0,0 +1,13 @@
+package es.ucm.fdi.iw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcmhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcmhApplication.class, args);
+	}
+
+}
---
+++ ucmh/src/test/java/es/ucm/fdi/iw/UcmhApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ucm.fdi.iw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcmhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
