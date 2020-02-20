fe7e39e405c14bab1b2c32f352e5494cd6ef6e7a
AlexCCo <canceloalejandro@gmail.com>
2020-02-20 14:23:03 +0100

    Added an index template

--- ucmh/src/main/java/es/fdi/ucm/ucmh/controller/RootController.java
+++ ucmh/src/main/java/es/fdi/ucm/ucmh/controller/RootController.java
@@ -2,13 +2,24 @@ package es.fdi.ucm.ucmh.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RootController {
 	
 	@GetMapping("/")
-	public String hola() {
+	public String indexPage() {
+		return "index";
+	}
+	
+	@GetMapping("/ajustesPaciente.html")
+	public String ajustesPaciente() {
 		return "ajustesPaciente";
 	}
-
+	
+	@GetMapping("/misCitas.html")
+	public String misCitas() {
+		return "misCitas";
+	}
+	
 }
