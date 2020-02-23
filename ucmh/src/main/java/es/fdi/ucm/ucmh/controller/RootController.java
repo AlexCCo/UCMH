d2bb69608f6d1562d3e1e73d7eceedd90f49b2c8
AlexCCo <canceloalejandro@gmail.com>
2020-02-23 15:18:06 +0100

    Error handling add and file hierarchy changed

--- ucmh/src/main/java/es/fdi/ucm/ucmh/controller/RootController.java
+++ ucmh/src/main/java/es/fdi/ucm/ucmh/controller/RootController.java
@@ -1,32 +1,43 @@
 package es.fdi.ucm.ucmh.controller;
 
+import es.fdi.ucm.ucmh.exceptionHandlers.ResourceNotFound;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RootController {
+	private static Logger log = LogManager.getLogger(RootController.class);
 	
-	@GetMapping("/")
-	public String indexPage() {
-		return "index";
+	@RequestMapping(value = "/{usersTemplates}", method = RequestMethod.GET)
+	public String indexPage(@PathVariable("usersTemplates") final String webUsers) {
+		if(webUsers.equals("ajustesPaciente.html")) {
+			return "ajustesPaciente";
+		}
+		else if(webUsers.equals("misCitas.html")) {
+			return "misCitas";
+		}
+		else if(webUsers.equals("estadisticasPaciente.html")) {
+			return "estadisiticasPaciente";
+		}
+		else if(webUsers.equals("")) {
+			return "index";
+		}
+		else {
+			throw new ResourceNotFound();
+		}
+	}
+
+	//ERRORS HANDLING
+	
+	@ExceptionHandler(ResourceNotFound.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String errorCode404NotFound() {
+		log.error("the client faild my boy");
+		return "404";
 	}
 	
-	@GetMapping("/ajustesPaciente.html")
-	public String ajustesPaciente() {
-		return "ajustesPaciente";
-	}
-	
-	@GetMapping("/misCitas.html")
-	public String misCitas() {
-		return "misCitas";
-	}
-	
-	@GetMapping("/estadisiticasPaciente.html")
-	public String estadisticasPaciente() {
-		return "estadisiticasPaciente";
-	}
-	@GetMapping("/miHorarioPsicologo.html")
-	public String miHorarioPsicologo() {
-		return "miHorarioPsicologo";
-	}
 }
