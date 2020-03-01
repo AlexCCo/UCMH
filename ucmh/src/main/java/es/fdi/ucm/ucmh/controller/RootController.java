c67c4a2359c25817cf06432b44072ab328815675
victornado <victornado@victornado>
2020-03-01 11:32:58 +0100

    doing java model

--- ucmh/src/main/java/es/fdi/ucm/ucmh/controller/RootController.java
+++ ucmh/src/main/java/es/fdi/ucm/ucmh/controller/RootController.java
@@ -1,12 +1,16 @@
 package es.fdi.ucm.ucmh.controller;
 
-import es.fdi.ucm.ucmh.exceptionHandlers.ResourceNotFound;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import es.fdi.ucm.ucmh.exceptionHandlers.ResourceNotFound;
 
 @Controller
 public class RootController {
