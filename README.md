98966f8e72c587fdc9fce2c39a9bfe993d4f7d1a
AlexCCo <canceloalejandro@gmail.com>
2020-03-01 12:56:02 +0100

    2.0 the returning of no c lo k ago

---
+++ README.md
@@ -0,0 +1,2 @@
+# UCMH
+UCMH Is a web project develop by some students to create a platform where it is easier to contact with the psychological personal of Universidad Complutense de Madrid
---
+++ ucmh/src/test/java/es/fdi/ucm/UCMH/UcmhApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fdi.ucm.UCMH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcmhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
